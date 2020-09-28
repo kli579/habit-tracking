f54331489d2314b91e8acdf76c2bebb305fcc8f6
Kathy Li <kathyluli94@gmail.com>
2020-09-28 14:29:12 -0400

    Fix eslint configuration

--- frontend/src/serviceWorker.js
+++ frontend/src/serviceWorker.js
@@ -1,3 +1,8 @@
+// Ignoring lint errors for this file since it was created by create-react-app
+/* eslint-disable no-console */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-param-reassign */
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
